77866b94436ff5f7e9501a3c27df906ba5c88b06
giovannizanettidasilva@gmail.com <giovannizanettidasilva@gmail.com>
2024-10-28 10:17:23 -0300

    aula navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, {useState} from 'react'
 import {Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
 
 const App = () => {
   const [color] = useState("#e4ff88")
---
+++ src/pages/Home.jsx
@@ -0,0 +1,49 @@
+import logo from '../assets/react.svg'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+ 
+const Home = () => {
+ 
+    return (
+        <>
+            <div>
+                 
+            <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+
+            </div>
+
+            <h1>Projeto Pizzaria</h1>
+
+            <div>
+                
+
+            </div>
+        </>
+    )
+}
+ 
+export default Home
